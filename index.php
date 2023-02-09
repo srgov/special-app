0d13431567acb737a41a074fc5e222477a2cc73d
Sergey <srg@yandex.ru>
2023-02-09 21:06:46 +0500

    bdo changed

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ define( 'WP_USE_THEMES', true );
 define( 'WP_USE_THEMES', true );
 define( 'WP_USE_THEMES', true );
 define( 'WP_USE_THEMES', true );
-rrrrrrrrrRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR
+basket-delivery-options
 
 /** Loads the WordPress Environment and Template */
 require __DIR__ . '/wp-blog-header.php';
