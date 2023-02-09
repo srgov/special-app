41ae8c447273bcf0ae7aebd6d9b69e7da786b1b6
Sergey <srg@yandex.ru>
2023-02-09 21:09:54 +0500

    bdo changed 2

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ new comment
  */
 define( 'WP_USE_THEMES', ture );
 define( 'WP_USE_THEMES', true );
-basket-delivery-options
+basket-delivery-options test
 
 /** Loads the WordPress Environment and Template */
 require __DIR__ . '/wp-blog-header.php';
